b14affb40d4908beac45c23c0f2e5aefb71efd21
Farhan Rafly Fahrezi <raflyfahr15@gmail.com>
2020-11-07 04:46:29 +0700

    split pages

---
+++ src/pages/add.jsx
@@ -0,0 +1,51 @@
+import React, { useRef, useState } from 'react'
+import { useRouter } from 'next/router'
+import Storage from '../redux/store'
+import { SET } from '../redux/action'
+
+const add = () => {
+    const Router = useRouter()
+
+    const InputNama = useRef()
+
+    const [ nama, setNama ] = useState({
+        type : SET(),
+        nama : null
+    })
+
+    const submitHandler = e => {
+        e.preventDefault()
+        
+        Storage.dispatch(nama)
+
+        InputNama.current.value = ''
+    }
+
+    const namaHandler = e => {
+        const Element = e.target
+        const Temp = { ...nama }
+
+        Temp['nama'] = Element['value']
+
+        setNama(Temp)
+    }
+
+    return (
+        <div>
+            <form onSubmit={ submitHandler }>
+                <label>Masukan Nama : </label>
+                <input 
+                    type="text" 
+                    name="nama" 
+                    id="nama" 
+                    onChange={ namaHandler }
+                    ref={ InputNama }
+                />
+                <button type="submit">Submit</button>
+            </form>
+            <button onClick={ () => Router.push('/') }>Home</button>
+        </div>
+    )
+}
+
+export default add
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,57 +1,16 @@
-import React, { useState, useEffect, useRef } from 'react'
-import { SET } from '../redux/action' 
+import React, { useState } from 'react'
+import { useRouter } from 'next/router'
 import Storage from '../redux/store'
 
 const index = () => {
-    const InputNama = useRef()
-
-    const [ nama, setNama ] = useState({
-        type : SET(),
-        nama : null
-    })
-
-    const [ stateChanged, setStateChanged ] = useState(0)
+    const Router = useRouter()
 
     const [ anotherName, setAnotherName ] = useState(Storage.getState())
 
-    useEffect(() => {
-        setAnotherName(Storage.getState())
-    }, [ stateChanged ])
-
-    const submitHandler = e => {
-        e.preventDefault()
-        
-        Storage.dispatch(nama)
-
-        InputNama.current.value = ''
-        setStateChanged(prev => prev + 1)
-    }
-
-    const namaHandler = e => {
-        const Element = e.target
-        const Temp = { ...nama }
-
-        Temp['nama'] = Element['value']
-
-        setNama(Temp)
-    }
-
     return (
         <div>
-            <div>
-                <form onSubmit={ submitHandler }>
-                    <label>Masukan Nama : </label>
-                    <input 
-                        type="text" 
-                        name="nama" 
-                        id="nama" 
-                        onChange={ namaHandler }
-                        ref={ InputNama }
-                    />
-                    <button type="submit">Submit</button>
-                </form>
-            </div>
             <p>Nama : { anotherName }</p>
+            <button onClick={ () => Router.push('/add') }>Tambah</button>
         </div>
     )
 }
