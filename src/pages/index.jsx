8badc53d7858d85d8444b7840b6e951ece13f6f3
Farhan Rafly Fahrezi <raflyfahr15@gmail.com>
2020-11-06 20:03:16 +0700

    create form

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const index = () => {
+    const [ nama, setNama ] = useState('')
+
+    const submitHandler = e => {
+        e.preventDefault()
+    }
+
+    const namaHandler = e => {
+        const Element = e.target
+
+        setNama(Element['value'])
+    }
+
     return (
         <div>
-      
+            <div>
+                <form onSubmit={ submitHandler }>
+                    <label>Masukan Nama : </label>
+                    <input type="text" name="nama" id="nama" onChange={ namaHandler }/>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+            <p>Nama : { nama }</p>
         </div>
     )
 }
