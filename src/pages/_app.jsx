0d6cb65af6c40d4e48b73d308a6762c6d796fc92
Farhan Rafly Fahrezi <raflyfahr15@gmail.com>
2020-11-09 19:52:31 +0700

    adding provider

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,8 +1,12 @@
+import { Provider } from 'react-redux'
+import Store from '../redux/store'
 import '../styles/globals.css'
 
 const MyApp = ({ Component, pageProps }) => {
     return (
-        <Component {...pageProps} />
+        <Provider store={ Store }>
+            <Component {...pageProps} />
+        </Provider>
     )
 }
 
