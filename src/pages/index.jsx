996a0f56d9230e104a6d7a15382907bc46424c87
Farhan Rafly Fahrezi <raflyfahr15@gmail.com>
2020-11-10 06:17:30 +0700

    styling index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,9 +6,23 @@ import { connect } from 'react-redux'
 const index = ({ name }) => {
     const Router = useRouter()
 
+    const Container = {
+        width : '100%',
+        maxWidth : '300px',
+        
+        m : '0 auto',
+        p : '30px 0',
+        
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: '30px'
+    }
+
     return (
-        <div>
-            <p>Nama : { name }</p>
+        <div sx={ Container }>
+            <h1>Current Name</h1>
+            <p>{ name }</p>
             <button onClick={ () => Router.push('/add') }>Tambah</button>
         </div>
     )
